c1cf860fc6fc9127882cc9666d6c306ffd34358a
Vishnu Satish <vishnupavan.satish@gmail.com>
2019-11-05 19:38:25 -0500

    Create Snapshot

---
+++ src/main.js
@@ -0,0 +1,47 @@
+const { app, BrowserWindow } = require('electron')
+
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 432,
+    height: 675,
+    fullscreen: false,
+    webPreferences: {
+      nodeIntegration: true,
+      webviewTag: true,
+      webSecurity: false,
+      allowRunningInsecureContent: true,
+    }
+  })
+
+  win.loadFile('index.html')
+
+  //win.webContents.openDevTools()
+
+  win.on('closed', () => {
+
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  
+  if (win === null) {
+    createWindow()
+  }
+})
+
